9a4fa407dd048565117c5c35764a70a70f6635c5
brettinternet <brettinternet@gmail.com>
2019-10-31 16:31:00 -0600

    move slide reset to end of call stack

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,9 @@ Reveal.initialize(options);
 const reveal = document.getElementById("reveal");
 reveal.style.opacity = "0";
 Reveal.addEventListener("ready", function(_event) {
-  Reveal.slide(0, 0, 0, 0);
+  setTimeout(function() {
+    Reveal.slide(0, 0, 0, 0);
+  }, 0);
   setTimeout(function() {
     reveal.style.transition = "opacity 500ms";
     reveal.style.opacity = "1";
