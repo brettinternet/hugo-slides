05a4667567458325681c44811c77e8f6d9452dd1
brettinternet <brettinternet@gmail.com>
2019-11-16 18:08:48 -0700

    add pre-commit script and hook through npm scripts

---
+++ bin/pre-commit.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const path = require("path");
+
+const appRootDir = path.join(__dirname, "..");
+const bundleDir = path.join(appRootDir, "assets", "bundle");
+
+/**
+ * Parcel produces JS files for watched CSS in the output directory
+ * If those exist we need to build our bundles for production
+ * and overwrite the development files
+ */
+const filePath = path.join(bundleDir, "css", "main.js");
+if (fs.existsSync(filePath)) {
+  require("./setup-bundle.js");
+}
